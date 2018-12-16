1f2549ae6c4c864ec0ef07abee6824f9d05f3ef5
Manuel de Jesús González Arvelo <manuelarvelo19@gmail.com>
2018-12-16 21:06:39 +0100

    pwa update

--- speed/src/app/app.component.ts
+++ speed/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { UpdateAvailableEvent} from '@angular/service-worker/src/low_level';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'speed';
+  public developerVersion = '6';
+  constructor(swUpdate: SwUpdate) {
+    if (swUpdate.isEnabled) {
+      swUpdate.available.subscribe(
+        (event: UpdateAvailableEvent) => {
+          const msg = `Current ${event.current.hash}. Load new: ${event.available.hash} ?`;
+          if (confirm(msg)) { window.location.reload(); }
+        }
+      );
+
+    }
+  }
 }
